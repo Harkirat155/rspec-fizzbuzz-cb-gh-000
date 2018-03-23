006509109b8b786d062ccdf694b4ef22648368d7
Harkirat Singh <harkirat.hira@gmail.com>
2018-03-23 15:16:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(x)
-  if x % 3 == 0
-    puts "Fizz"
-  elsif x % 5 == 0
-    puts "Buzz"
-  else ((x % 3 == 0) && (x % 5 == 0))
-    puts "FizzBuzz"
-  end
+ x = x.to_i
+ if x % 3 == 0
+   puts "fizz"
+   if x % 5 == 0
+     puts "fizzbuzz" 
+ elsif x % 5 == 0
+   puts "buzz"
+ else 
+   puts "nil"
 end
