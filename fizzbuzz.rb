3077076a7f1f5ba7ee4707ab4f38639013115848
Harkirat Singh <harkirat.hira@gmail.com>
2018-03-23 15:37:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,10 @@ def fizzbuzz(x)
   x = x.to_i
   if x % 3 == 0
     return "Fizz"
-    if x % 5 == 0
-      return "FizzBuzz"
-    end
   elsif x % 5 == 0
     return "Buzz"
+  elsif x % 3 == 0 && x % 5 == 0
+    return "FizzBuzz"
   else
     return nil
   end
